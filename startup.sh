cbb08882299d70a65a5654d85c3c55d65b7dfd0a
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-09 14:50:14 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
-cat bitnami_application_password
+$pw=`cat bitnami_application_password`
+
+# create database users and schemas
+mysql -u root -p
 
 # directory for php files
 #/opt/bitnami/apache2/htdocs
