f9754d7f8fc58ca212011b381b5f74424a5fa40a
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-11 15:38:43 -0600

    Update create_plog.sql

--- create_plog.sql
+++ create_plog.sql
@@ -6,7 +6,8 @@ CREATE TABLE item (
   datestamp datetime null,
   sort_idx int null,
   last_mod_date datetime not null default now(),
-  plog_id int null,
+  plog_pk int null,
+  plog_parent_pk int null,
   constraint pk_example primary key (item_id)
 );
 
@@ -14,8 +15,6 @@ CREATE TABLE join (
   join_id int unsigned not null auto_increment,
   item_id int not null,
   parent_id int not null,
-  plog_id int null,
-  plog_parent_id int null,
   last_mod_date datetime not null default now(),
   constraint pk_example primary key (join_id)
 );
