9c361af40a7aeafb02580b80ccac9479ad9c219c
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-11 15:39:54 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@ $pw=`cat bitnami_application_password`
 
 # create database users and schemas
 mysql -u root -p
+#GRANT ALL PRIVILEGES ON *.* TO 'plog'@'localhost' IDENTIFIED BY 'password';
 
 # directory for php files
 #/opt/bitnami/apache2/htdocs
