c143845451b5f24f62dbabd5d814d6676df285a1
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-11 15:45:54 -0600

    cleaned up sql for keyword issues

--- create_plog.sql
+++ create_plog.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE plog;
 USE plog;
-CREATE TABLE item (
+
+CREATE TABLE items (
   item_id int unsigned not null auto_increment,
-  desc varchar(20) not null,
+  description varchar(20) not null,
   datestamp datetime null,
   sort_idx int null,
   last_mod_date datetime not null default now(),
@@ -11,7 +12,7 @@ CREATE TABLE item (
   constraint pk_example primary key (item_id)
 );
 
-CREATE TABLE join (
+CREATE TABLE joins (
   join_id int unsigned not null auto_increment,
   item_id int not null,
   parent_id int not null,
