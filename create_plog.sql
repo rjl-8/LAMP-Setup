208597976cb0102545d2e12f2623a1d38089a121
rjl-8 <34580339+rjl-8@users.noreply.github.com>
2019-02-11 14:31:09 -0600

    Update create_plog.sql

--- create_plog.sql
+++ create_plog.sql
@@ -3,9 +3,10 @@ USE plog;
 CREATE TABLE item (
   item_id int unsigned not null auto_increment,
   desc varchar(20) not null,
-  the_date datetime not null,
+  the_date datetime null,
   creation_date datetime not null,
   last_mod_date datetime not null,
+  plog_id int null,
   constraint pk_example primary key (item_id)
 );
 
@@ -13,6 +14,8 @@ CREATE TABLE join (
   join_id int unsigned not null auto_increment,
   item_id int not null,
   parent_id int not null,
+  plog_id int null,
+  plog_parent_id int null,
   creation_date datetime not null,
   constraint pk_example primary key (join_id)
 );
